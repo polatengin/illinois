4cb0dc8982d5e7f91435949c46515f1038043d50
polatengin <enpolat@microsoft.com>
2023-09-26 20:22:06 +0000

    adding root command

--- src/Program.cs
+++ src/Program.cs
@@ -4,5 +4,7 @@ internal class Program
 {
   private static async Task<int> Main(string[] args)
   {
+    var rootCommand = new RootCommand("Sample app for System.CommandLine");
+    return await rootCommand.InvokeAsync(args);
   }
 }
