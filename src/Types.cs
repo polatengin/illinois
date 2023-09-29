be8fc82fe3c7f02c4f06d9eb3bb7a2fd17355cda
polatengin <enpolat@microsoft.com>
2023-09-29 17:40:43 +0000

    bicep types updated

--- src/Types.cs
+++ src/Types.cs
@@ -3,6 +3,4 @@ using System.Text.Json.Serialization;
 
 public record Root([property: JsonPropertyName("$schema")]string schema, string languageVersion, string contentVersion, Metadata metadata, JsonElement parameters, JsonElement variables, JsonElement resources, JsonElement outputs);
 
-public record Metadata(string _EXPERIMENTAL_WARNING, IReadOnlyList<string> _EXPERIMENTAL_FEATURES_ENABLED, Generator _generator, string description);
-
-public record Generator(string name, string version, string templateHash);
+public record Metadata(string _EXPERIMENTAL_WARNING, IReadOnlyList<string> _EXPERIMENTAL_FEATURES_ENABLED);
