b625719b6fd2c3ff83b5f51f0f7ffc2c09d207b4
polatengin <enpolat@microsoft.com>
2023-09-29 00:23:01 +0000

    creating a commandline option

--- src/Program.cs
+++ src/Program.cs
@@ -11,6 +11,7 @@ internal class Program
   {
     var serveOption = new Option<bool>(aliases: new[] { "--serve" }, description: "The serve to read and display on the console");
     var sortOption = new Option<bool>(aliases: new[] { "--sort" }, description: "Sort members alphabetically");
+    var outputFormatOption = new Option<Format>(aliases: new[] { "--output-format" }, description: "The format to use for the output");
     return await rootCommand.InvokeAsync(args);
   }
 }
