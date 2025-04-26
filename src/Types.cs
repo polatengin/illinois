9147394056834b6cfd8631bfdcc39b7dc572f903
Engin Polat <enpolat@microsoft.com>
2025-04-26 15:19:36 -0700

    fix: restore original Root record structure and remove Metadata type

--- src/Types.cs
+++ src/Types.cs
@@ -1,6 +1,5 @@
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 
-public record Root([property: JsonPropertyName("$schema")]string schema, string languageVersion, string contentVersion, Metadata metadata, JsonElement parameters, JsonElement variables, JsonElement resources, JsonElement outputs);
-
-public record Metadata(string _EXPERIMENTAL_WARNING, IReadOnlyList<string> _EXPERIMENTAL_FEATURES_ENABLED);
+public record Root([property: JsonPropertyName("$schema")]string schema, string languageVersion, string contentVersion, JsonElement metadata, JsonElement definitions, JsonArray functions, JsonElement parameters, JsonElement variables, JsonElement resources, JsonElement outputs);
